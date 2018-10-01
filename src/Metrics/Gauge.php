ae1e975123180cbbe7fd1d86f4736d3ba725ee58
Holger Woltersdorf <hw@hollo.me>
2018-10-01 21:00:43 +0200

    Remove metric type, metric name, metric help
    * Move type and help to collection
    * Metric name already exists in collection
    * Rename named constructors
    * Remove ability to return single metric string

--- src/Metrics/Gauge.php
+++ src/Metrics/Gauge.php
@@ -3,18 +3,11 @@
 namespace OpenMetricsPhp\Exposition\Text\Metrics;
 
 use OpenMetricsPhp\Exposition\Text\Collections\LabelCollection;
-use OpenMetricsPhp\Exposition\Text\Interfaces\NamesMetric;
 use OpenMetricsPhp\Exposition\Text\Interfaces\ProvidesNamedValue;
 use function sprintf;
-use function str_replace;
 
 final class Gauge
 {
-	private const TYPE = 'gauge';
-
-	/** @var NamesMetric */
-	private $metricName;
-
 	/** @var float */
 	private $gaugeValue;
 
@@ -24,48 +17,45 @@ final class Gauge
 	/** @var LabelCollection */
 	private $labels;
 
-	/** @var string */
-	private $help;
-
 	/**
-	 * @param NamesMetric $metricName
-	 * @param float       $gaugeValue
-	 * @param int|null    $timestamp
+	 * @param float    $gaugeValue
+	 * @param int|null $timestamp
 	 */
-	private function __construct( NamesMetric $metricName, float $gaugeValue, ?int $timestamp = null )
+	private function __construct( float $gaugeValue, ?int $timestamp = null )
 	{
-		$this->metricName = $metricName;
 		$this->gaugeValue = $gaugeValue;
 		$this->timestamp  = $timestamp;
 		$this->labels     = LabelCollection::new();
-		$this->help       = '';
 	}
 
-	/**
-	 * @param NamesMetric $metricName
-	 * @param float       $gaugeValue
-	 *
-	 * @return Gauge
-	 */
-	public static function fromMetricNameAndValue( NamesMetric $metricName, float $gaugeValue ) : self
+	public static function fromValue( float $gaugeValue ) : self
 	{
-		return new self( $metricName, $gaugeValue );
+		return new self( $gaugeValue );
 	}
 
-	/**
-	 * @param NamesMetric $metricName
-	 * @param float       $gaugeValue
-	 * @param int         $timestamp
-	 *
-	 * @return Gauge
-	 */
-	public static function fromMetricNameValueAndTimestamp(
-		NamesMetric $metricName,
+	public static function fromValueAndTimestamp(
 		float $gaugeValue,
 		int $timestamp
 	) : self
 	{
-		return new self( $metricName, $gaugeValue, $timestamp );
+		return new self( $gaugeValue, $timestamp );
+	}
+
+	public function withLabels( ProvidesNamedValue $label, ProvidesNamedValue ...$labels ) : self
+	{
+		$this->addLabels( $label, ...$labels );
+
+		return $this;
+	}
+
+	public function withLabelCollection( LabelCollection $labels ) : self
+	{
+		foreach ( $labels->getIterator() as $label )
+		{
+			$this->addLabels( $label );
+		}
+
+		return $this;
 	}
 
 	public function addLabels( ProvidesNamedValue $label, ProvidesNamedValue ...$labels ) : void
@@ -73,49 +63,13 @@ final class Gauge
 		$this->labels->add( $label, ...$labels );
 	}
 
-	public function setHelp( string $helpText ) : void
-	{
-		$this->help = str_replace( "\n", ' ', trim( $helpText ) );
-	}
-
-	public function getHelpString() : string
-	{
-		if ( '' === $this->help )
-		{
-			return '';
-		}
-
-		return sprintf( '# HELP %s %s', $this->metricName->toString(), $this->help );
-	}
-
-	public function getMetricName() : NamesMetric
-	{
-		return $this->metricName;
-	}
-
-	public function getTypeString() : string
-	{
-		return sprintf( '# TYPE %s %s', $this->metricName->toString(), self::TYPE );
-	}
-
 	public function getSampleString() : string
 	{
 		return sprintf(
-			'%s%s %f%s',
-			$this->metricName->toString(),
+			'%s %f%s',
 			$this->labels->getCombinedLabelString(),
 			$this->gaugeValue,
 			null !== $this->timestamp ? (' ' . $this->timestamp) : ''
-		);
-	}
-
-	public function getSingleMetricString() : string
-	{
-		return sprintf(
-			"%s%s\n%s",
-			'' !== $this->getHelpString() ? "{$this->getHelpString()}\n" : '',
-			$this->getTypeString(),
-			$this->getSampleString()
 		);
 	}
 }
