9cfb5680a4bc3a3b401b16e68b8df271fc4010e6
Holger Woltersdorf <hw@hollo.me>
2018-11-01 16:14:49 +0100

    Add re-constructor for suffixing a metric name

--- src/Interfaces/NamesMetric.php
+++ src/Interfaces/NamesMetric.php
@@ -7,4 +7,6 @@ interface NamesMetric
 	public function toString() : string;
 
 	public function equals( NamesMetric $other ) : bool;
+
+	public function withSuffix( string $suffix ) : NamesMetric;
 }
