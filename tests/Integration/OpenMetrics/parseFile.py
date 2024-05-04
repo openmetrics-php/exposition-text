5bf4a850626df93a156715e97c3183fd6bad84ae
Holger Woltersdorf <hw@hollo.me>
2024-05-04 12:22:38 +0200

    Simplify data input for prometheus client

--- tests/Integration/OpenMetrics/parseFile.py
+++ tests/Integration/OpenMetrics/parseFile.py
@@ -1,13 +1,8 @@
 from prometheus_client.openmetrics.parser import text_string_to_metric_families
 import sys
 
-if sys.version_info[0] == 3:
-    from path import Path
-    metrics = Path(sys.argv[1]).bytes().decode("utf-8")
-else:
-    from path import path
-    metrics = path(sys.argv[1]).bytes()
+input = sys.stdin.read()
 
-for family in text_string_to_metric_families(metrics):
+for family in text_string_to_metric_families(input):
   for sample in family.samples:
     print("Name: {0} Labels: {1} Value: {2} Timestamp: {3}".format(*sample))
